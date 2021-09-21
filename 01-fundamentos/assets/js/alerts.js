0c8621fa49e9a55666618a8424121de61b8cc0fd
Eduardo <eduardoromeror9@outlook.com>
2021-09-21 04:47:10 -0300

    JavaScript fundamentos ok

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,13 @@
+
+
+
+// alert('Hola Mundo');
+
+// let nombre = prompt('Ingresa tu nombre: ');
+// console.log(nombre);
+// console.log('****' + nombre + '****');
+
+// const seleccion = confirm('¿Estas seguro de querer continuar?');
+// console.log(seleccion);
+
+console.log(global);
