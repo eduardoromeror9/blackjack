b5cb8ab4b1f14d8abe65d72312681933eeaa013b
Eduardo Romero <eduardoromeror9@outlook.com>
2021-09-22 07:27:16 -0300

    arreglos JS

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,13 @@
+// const arr = new Array(10);
+
+let videoJuegos = ['Mario3', 'Mega Man', 'Chrono Trigger'];
+
+let arregloCosas = [
+   true,
+   false,
+   'Hola',
+   'Eduardo',
+   1 + 2
+];
+
+console.log(arregloCosas);
---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,27 @@
+let enume = 11233;
+// class
+// let
+// var
+// const
+// function
+// return
+// if
+// else
+// for
+// while
+// do
+// switch
+// case
+// break
+// default
+// continue
+// try
+// catch
+// finally
+
+/**
+ * Asterisco
+ * numeral
+ * numeral
+ * enter 
+ */
