1c6ec06d328a1022d84a41a53fdd450a81027e2d
Eduardo Romero <eduardoromeror9@outlook.com>
2021-09-30 03:13:33 -0300

    Blackjack

---
+++ 01-fundamentos/assets/js/ciclos/for.js
@@ -0,0 +1,19 @@
+const heroes = ['Flash', 'Arrow', 'Superman', 'Batman', 'Mujer Maravilla'];
+
+console.warn('For tradicional')
+
+for (let i = 0; i < heroes.length; i++) {
+    console.log(heroes[i]);
+}
+
+console.warn('For in')
+
+for (let i in heroes) {
+    console.log(heroes[i]);
+}
+
+
+console.warn('For of')
+for (let heroe of heroes) {
+    console.log(heroe);
+}
